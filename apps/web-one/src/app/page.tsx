30267f19bd107ddd426dfcddad06f18335197a07
Peter van Meijgaard <petervanmeijgaard@gmail.com>
2024-03-23 00:18:30 +0100

    Initial commit

---
+++ apps/web-one/src/app/page.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+	return (
+		<main className="p-4 rounded border-2 border-gray-400">
+			<h1 className="flex text-2xl font-bold text-gray-600">@acme/web-one</h1>
+		</main>
+	);
+}
