a0722f94cdcccbe5da4badda009f2f6d70c0f804
Peter van Meijgaard <petervanmeijgaard@gmail.com>
2024-03-23 00:47:08 +0100

    Update brand color to purple

--- apps/web-two/src/app/page.tsx
+++ apps/web-two/src/app/page.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
 	return (
-		<main className="p-4 rounded border-2 border-gray-400">
-			<h1 className="flex text-2xl font-bold text-gray-600">@acme/web-two</h1>
+		<main className="p-4 rounded border-2 border-purple-400">
+			<h1 className="flex text-2xl font-bold text-purple-600">@acme/web-two</h1>
 		</main>
 	);
 }
